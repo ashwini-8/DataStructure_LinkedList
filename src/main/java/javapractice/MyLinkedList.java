3d15b4b4721dd0fd8d83c692235b4cebd8cb8be3
ashwini-8 <ashwinip309@gmail.com>
2021-01-05 10:52:56 +0530

    Added:creating Linkedlist by inserting 30 between 56 and 70

--- src/main/java/javapractice/MyLinkedList.java
+++ src/main/java/javapractice/MyLinkedList.java
@@ -45,4 +45,11 @@ public class MyLinkedList {
             this.tail = myNode;
         }
     }
+
+    public void insert(INode myNode ,INode newNode) {
+        INode tempNode = myNode.getNext();
+        myNode.setNext(newNode);
+        newNode.setNext(tempNode);
+
+    }
 }
--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -39,4 +39,20 @@ public class MyLinkedListTest {
 
 
     }
+
+    @Test
+    public void given3NumbersWhenInsertSecondInBetweenShouldPassLinkedListResult() {
+        MyNode<Integer> myfirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myfirstNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.insert(myfirstNode,mySecondNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.head.equals(myfirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
